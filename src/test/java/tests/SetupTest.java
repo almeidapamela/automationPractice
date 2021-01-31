0d5399e0bde58df506534ba5cf743d86d26b180f
almeidapamela <pamela.alves.almeida@gmail.com>
2021-01-31 19:31:16 -0300

    commit inicial

---
+++ src/test/java/tests/SetupTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.Test;
+import utils.Browser;
+import utils.Utils;
+import static org.junit.Assert.assertTrue;
+
+public class SetupTest extends BaseTests {
+    @Test
+    public void testOpenBrowserAndLoadingPage(){
+        assertTrue(Browser.getCurrentDriver().getCurrentUrl().contains(Utils.getBaseUrl()));
+        System.out.println("Abrimos navegador e carregamos a URL!");
+    }
+}
